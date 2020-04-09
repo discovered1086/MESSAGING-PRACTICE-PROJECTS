dc4e355e80aaa9abb9273f96ac36f62dfc07c527
Kingshuk Mukherjee <sly.mania42@gmail.com>
2020-04-08 21:30:59 -0500

    Creating the project

---
+++ first-spring-jms/src/main/java/com/kingshuk/messaging/springjms/FirstSpringJmsApplication.java
@@ -0,0 +1,13 @@
+package com.kingshuk.messaging.springjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringJmsApplication.class, args);
+	}
+
+}
