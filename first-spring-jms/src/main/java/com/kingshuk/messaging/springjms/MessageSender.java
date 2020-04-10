1f2af10eb1eba321aca01836e97b9278e824fc8e
Kingshuk Mukherjee <sly.mania42@gmail.com>
2020-04-09 19:37:41 -0500

    Small changes

--- first-spring-jms/src/main/java/com/kingshuk/messaging/springjms/MessageSender.java
+++ first-spring-jms/src/main/java/com/kingshuk/messaging/springjms/MessageSender.java
@@ -3,6 +3,7 @@ package com.kingshuk.messaging.springjms;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
@@ -10,16 +11,19 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 @Slf4j
 public class MessageSender {
-	
+
 	@Autowired
 	private JmsTemplate jmsTemplate;
-	
+
 	@Value("${springjms.queueName}")
 	private String queueName;
-	
+
 	public void sendMessage(String message) {
 		log.info("Inside the message sender method.....");
-		jmsTemplate.convertAndSend(queueName,message);
+
+		MessageCreator messageCreator = session -> session.createTextMessage(message);
+
+		jmsTemplate.send(queueName, messageCreator);
 	}
 
 }
